5a400b2da81cbb52947ca70b37831a5bd83e7334
Jasurbek <ismoilov.dzhasurbek02@gmail.com>
2023-11-01 14:42:53 +0500

    CreateCarMapper.cs and CreateCarCommandHandler.cs files added

---
+++ CarCatalog.Application/Features/Cars/Commands/CreateCar/CreateCarCommandHandler.cs
@@ -0,0 +1,28 @@
+using AutoMapper;
+using CarCatalog.Application.Common.Interfaces;
+using CarCatalog.Domain.Entities;
+using MediatR;
+
+namespace CarCatalog.Application.Features.Cars.Commands.CreateCar;
+
+public class CreateCarCommandHandler : IRequestHandler<CreateCarCommand, Guid>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public CreateCarCommandHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<Guid> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+    {
+        var car = _mapper.Map<Car>(request);
+
+        _context.Cars.Add(car);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return car.Id;
+    }
+}
