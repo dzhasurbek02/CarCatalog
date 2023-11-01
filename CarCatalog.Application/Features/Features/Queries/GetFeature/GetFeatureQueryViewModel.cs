586b1ccefd9e8a9eb035ac76fb96ee32963f4f52
Jasurbek <ismoilov.dzhasurbek02@gmail.com>
2023-11-01 13:20:57 +0500

    GetFeatureQuery.cs and GetFeatureQueryViewModel.cs files added

---
+++ CarCatalog.Application/Features/Features/Queries/GetFeature/GetFeatureQueryViewModel.cs
@@ -0,0 +1,10 @@
+namespace CarCatalog.Application.Features.Features.Queries.GetFeature;
+
+public class GetFeatureQueryViewModel
+{
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public DateTime CreatedDateTime { get; set; }
+}
